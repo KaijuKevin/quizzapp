98cf118dc3c70de25042b321ba33d1f4ec0663c0
Kevin <kevc.dev@gmail.com>
2015-07-28 12:16:17 -0700

    Create answers model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727162225) do
+ActiveRecord::Schema.define(version: 20150728185536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.integer  "quiz_id"
     t.text     "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "field_type"
   end
 
   add_index "questions", ["quiz_id"], name: "index_questions_on_quiz_id", using: :btree
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe Question do
 	it { should belong_to :quiz }	
+	it { should have_many :answers }
 	it { should validate_presence_of :title}
 end
